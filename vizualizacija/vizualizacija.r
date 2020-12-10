08a9d08541f4908796eb59b8d29c8efbfae14d48
gapo8 <gasper1999@gmail.com>
2020-12-10 15:37:17 +0100

    popravil zemljevid in dopolnil rmarkdown

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -9,5 +9,6 @@ svet_stevilo_igralcev <- left_join(x = World, y = stevilo_igralcev, by = c('name
 
 svet_stevilo_igralcev[is.na(svet_stevilo_igralcev)] <- 0 #spremenim na v 0
 
+tmap_mode('view')
 zemljevid_stevila_igralcev <- tm_shape(svet_stevilo_igralcev) + tm_polygons("stevilo", breaks = c(0, 1, 5, 10, 15, 20, 30, 40, Inf))
 
