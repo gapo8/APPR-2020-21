23abba06ade35c0820b40873ea6ecd2e58f9eef3
gapo8 <gasper1999@gmail.com>
2020-12-23 19:13:39 +0100

    začel z analizo

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,11 +1,20 @@
 # 4. faza: Analiza podatkov
 
-podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-                                gostota.naselij=naselja/povrsina) %>%
-  left_join(povprecja, by="obcina")
-row.names(podatki) <- podatki$obcina
-podatki$obcina <- NULL
 
-# Število skupin
-n <- 5
-skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+x <- povprecje_osebni_podatki
+x$COUNTRY[x$COUNTRY != 'United States'] <- 'druga'
+x <- x[!is.na(x$COUNTRY),]  #odstranimo vrstice z NA
+
+risba <- x %>% 
+  ggplot(aes(x=PTS, y=REB, col=COUNTRY)) + 
+  geom_point() +
+  geom_text(
+    data=x %>% mutate(n = row_number()), 
+    aes(label=n),
+    size=2,
+    color="black",
+    nudge_x = 0.03,
+    nudge_y = 0.03
+  ) + theme_minimal()
+
+risba + ggsave("risba.pdf", device="pdf")
